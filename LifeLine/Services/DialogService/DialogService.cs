6327ac9b699424766f67cd5e92e10a3a9ff22389
texno stalin <texno.stalin@gmail.com>
2024-09-13 13:57:22 +0300

    Создали диалоговый сервис

---
+++ LifeLine/Services/DialogService/DialogService.cs
@@ -0,0 +1,27 @@
+﻿using LifeLine.MVVM.View.Windows;
+using LifeLine.Utils.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeLine.Services.DialogService
+{
+    public class DialogService : IDialogService
+    {
+        public bool ShowMessage(string Message, string Title = "Предупреждение", MessageButtons messageButtons = MessageButtons.OK)
+        {
+            NotificationWindow notificationWindow = new(Message, Title, messageButtons);
+            notificationWindow.ShowDialog();
+            return notificationWindow.Result;
+        }
+
+        public MessageButtons ShowMessageButton(string Message, string Title = "Предупреждение", MessageButtons messageButtons = MessageButtons.OK)
+        {
+            NotificationWindow notificationWindow = new(Message, Title, messageButtons);
+            notificationWindow.ShowDialog();
+            return notificationWindow.ResultButton;
+        }
+    }
+}
---
+++ LifeLine/Services/DialogService/IDialogService.cs
@@ -0,0 +1,16 @@
+﻿using LifeLine.Utils.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeLine.Services.DialogService
+{
+    interface IDialogService
+    {
+        bool ShowMessage(string Message, string Title = "Предупреждение", MessageButtons messageButtons = MessageButtons.OK);
+        MessageButtons ShowMessageButton(string Message, string Title = "Предупреждение", MessageButtons messageButtons = MessageButtons.OK);
+    }
+}
