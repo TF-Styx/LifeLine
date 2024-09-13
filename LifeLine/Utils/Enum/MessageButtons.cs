d4d661cb044bbccc191a4f8182d8adc759c081c7
texno stalin <texno.stalin@gmail.com>
2024-09-13 14:01:21 +0300

    Забыл добавить код для окна уведомлений P.S. Не судите строго (^・ω・^ )

---
+++ LifeLine/Utils/Enum/MessageButtons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeLine.Utils.Enum
+{
+    public enum MessageButtons
+    {
+        OK = 0,
+        Cancel = 1,
+        OKCancel = 2,
+        Yes = 3,
+        No = 4,
+        YesNo = 5,
+        YesNoCancel = 6,
+    }
+}
