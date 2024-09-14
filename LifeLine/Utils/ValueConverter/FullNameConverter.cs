91030929e32431f9d0c209fe5390d8c22f0633ce
texno stalin <texno.stalin@gmail.com>
2024-09-14 17:59:14 +0300

    Добавлены конверторы

---
+++ LifeLine/Utils/ValueConverter/FullNameConverter.cs
@@ -0,0 +1,31 @@
+﻿using LifeLine.MVVM.Models.MSSQL_DB;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LifeLine.Utils.ValueConverter
+{
+    class FullNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Employee employee)
+            {
+                return $"{employee.SecondName} {employee.FirstName} {employee.LastName}";
+            }
+            else
+            {
+                return "Сотрудник";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LifeLine/Utils/ValueConverter/SecondNameToForegroundConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LifeLine.Utils.ValueConverter
+{
+    class SecondNameToForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string FirstLetter)
+            {
+                if (FirstLetter.StartsWith("С"))
+                {
+                    return (Brush)new BrushConverter().ConvertFromString("Red");
+                }
+                else
+                {
+                    return (Brush)new BrushConverter().ConvertFromString("Black");
+                }
+            }
+            else
+            {
+                return (Brush)new BrushConverter().ConvertFromString("Black");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
