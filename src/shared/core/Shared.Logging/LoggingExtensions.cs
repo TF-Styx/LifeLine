999b90972953408c0eb76ac67354693888f533bf
texno stalin <texno.stalin@gmail.com>
2025-10-04 16:26:09 +0300

    feat(логирование): добавить общую библиотеку Shared.Logging на базе Serilog

---
+++ src/shared/core/Shared.Logging/LoggingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Shared.Logging
+{
+    public static class LoggingExtensions
+    {
+        public static IHostBuilder UseSerialog(this IHostBuilder builder)
+        {
+            builder.UseSerilog((context, services, conf) =>
+            {
+                conf.MinimumLevel.Information()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                    .MinimumLevel.Override("System", LogEventLevel.Warning);
+
+                conf.Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .Enrich.WithThreadId()
+                    .Enrich.WithProperty("ApplicationName", context.HostingEnvironment.ApplicationName)
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+
+                conf.ReadFrom.Configuration(context.Configuration);
+            });
+
+            return builder;
+        }
+    }
+}
