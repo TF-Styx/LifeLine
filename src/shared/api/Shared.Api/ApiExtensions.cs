de0b8fa6702fd51723b026010e92bc34ae681da6
texno stalin <texno.stalin@gmail.com>
2025-10-07 13:42:45 +0300

    feat(files): реализовать передачу файлов в CQRS команды
    Для стандартизации работы с файлами и отделения доменной логики от инфраструктуры ASP.NET Core был введен специальный класс FileInput.
    
    - Добавлен класс FileInput, который содержит поток (Stream) и метаданные файла (имя, тип контента). Он будет использоваться в качестве параметра для CQRS команд.
    - Создан новый общий проект `Shared.Api` для переиспользуемых компонентов, связанных со слоем API.
    - В Shared.Api добавлен метод расширения `ToFileInput()` для удобного и единого преобразования `IFormFile` в `FileInput` прямо в контроллерах.

---
+++ src/shared/api/Shared.Api/ApiExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.Contracts.Request.Shared;
+
+namespace Shared.Api
+{
+    public static class ApiExtensions
+    {
+        public static FileInput? ToFileInput(this IFormFile? formFile)
+        {
+            FileInput? imageInput = null;
+            if (formFile is not null)
+            {
+                imageInput = new FileInput(
+                    formFile.OpenReadStream(),
+                    formFile.FileName,
+                    formFile.ContentType);
+
+                return imageInput;
+            }
+
+            return imageInput;
+        }
+    }
+}
---
+++ src/shared/core/Shared.Contracts/Request/Shared/FileInput.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.Contracts.Request.Shared
+{
+    public sealed record FileInput(Stream Content, string FileName, string ContentType);
+}
