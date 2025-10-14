a7fc65b4576dff2a8f1ef10dd55278833d721002
texno stalin <texno.stalin@gmail.com>
2025-10-14 13:59:53 +0300

    feat(employee-type): добавить DTO для управления типами занятости
    Для сущности EmployeeType были созданы модели запросов и ответа, чтобы отделить доменную модель от контрактов API.
    
    - Создан `CreateEmployeeTypeRequest` для обработки запросов на создание нового типа занятости.
    - Создан `UpdateEmployeeTypeRequest` для обработки запросов на обновление существующего типа.
    - Создан `EmployeeTypeResponse` для стандартизации и возврата данных клиенту.

---
+++ src/shared/core/Shared.Contracts/Request/EmployeeService/EmployeeType/CreateEmployeeTypeRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.Contracts.Request.EmployeeService.EmployeeType
+{
+    public sealed record CreateEmployeeTypeRequest(string Name, string Description);
+}
---
+++ src/shared/core/Shared.Contracts/Response/EmployeeService/EmployeeTypeResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.Contracts.Response.EmployeeService
+{
+    public sealed record EmployeeTypeResponse(Guid Id, string Name, string Description);
+}
