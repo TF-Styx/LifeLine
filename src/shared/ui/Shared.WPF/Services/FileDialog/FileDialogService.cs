be7ee700e482b70eb2de14d129cd536baeb1b602
texno stalin <texno.stalin@gmail.com>
2026-03-07 15:17:18 +0300

    feat: Реализовал FileDialogService для выбора и загрузки документов в приложение

---
+++ src/shared/ui/Shared.WPF/Services/FileDialog/FileDialogService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace Shared.WPF.Services.FileDialog
+{
+    public sealed class FileDialogService : IFileDialogService
+    {
+        public string GetFile(string title, string filter)
+        {
+            try
+            {
+                var dialog = new OpenFileDialog()
+                {
+                    Title = title,
+                    Filter = filter,
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    Multiselect = false
+                };
+
+                if (dialog.ShowDialog() == true)
+                    return dialog.FileName;
+
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при открытии файла: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return string.Empty;
+            }
+        }
+
+        public IEnumerable<string> GetFiles(string title, string filter)
+        {
+            try
+            {
+                var dialog = new OpenFileDialog()
+                {
+                    Title = title,
+                    Filter = filter,
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    Multiselect = true
+                };
+
+                if (dialog.ShowDialog() == true)
+                    return dialog.FileNames;
+
+                return [];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при открытии файлов: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return [];
+            }
+        }
+    }
+}
---
+++ src/shared/ui/Shared.WPF/Services/FileDialog/IFileDialogService.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.WPF.Services.FileDialog
+{
+    public interface IFileDialogService
+    {
+        string GetFile(string title, string filter);
+        IEnumerable<string> GetFiles(string title, string filter);
+    }
+}
