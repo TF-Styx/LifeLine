dc5f06d7743ab6fb39280c4e2cb29da01e95b942
texno stalin <texno.stalin@gmail.com>
2026-03-13 22:31:13 +0300

    feat: Создан UserControl для аватара сотрудника

---
+++ src/apps/LifeLine.HrPanel.Desktop/ViewModels/Features/AvatarVM.cs
@@ -0,0 +1,38 @@
+﻿using Shared.WPF.Commands;
+using Shared.WPF.Constants;
+using Shared.WPF.Helpers;
+using Shared.WPF.Services.FileDialog;
+using Shared.WPF.ViewModels.Abstract;
+using System.Windows.Media;
+
+namespace LifeLine.HrPanel.Desktop.ViewModels.Features
+{
+    internal sealed class AvatarVM : BaseViewModel
+    {
+        private readonly IFileDialogService _fileDialogService;
+
+        public AvatarVM(IFileDialogService fileDialogService)
+        {
+            _fileDialogService = fileDialogService;
+
+            SelectCommand = new RelayCommand(Execute_SelectCommand);
+        }
+
+        public ImageSource? Ava
+        {
+            get => field;
+            set => SetProperty(ref field, value);
+        }
+
+        private string? _path;
+
+        public RelayCommand? SelectCommand { get; private set; }
+        private void Execute_SelectCommand()
+        {
+            _path = _fileDialogService.GetFile($"Выберите файл: {FileDialogConsts.AVATAR}", FileFilters.Images);
+            Ava = ImageHelper.ToImageFromFilePath(_path);
+        }
+
+        public string? GetPath() => _path;
+    }
+}
