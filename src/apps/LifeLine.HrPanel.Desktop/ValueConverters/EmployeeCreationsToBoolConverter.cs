133802b905d810178d922663a028ffe0bee828cc
texno stalin <texno.stalin@gmail.com>
2026-03-13 22:36:12 +0300

    feat: Реализован ValueConverter для более удобного взаимодействия с Expander

---
+++ src/apps/LifeLine.HrPanel.Desktop/ValueConverters/EmployeeCreationsToBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using LifeLine.HrPanel.Desktop.Enums;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LifeLine.HrPanel.Desktop.ValueConverters
+{
+    internal class EmployeeCreationsToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is EmployeeCreationSteps valueSteps && parameter is EmployeeCreationSteps parameterSteps)
+            {
+                if (valueSteps == parameterSteps)
+                    return true;
+
+                return false;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
