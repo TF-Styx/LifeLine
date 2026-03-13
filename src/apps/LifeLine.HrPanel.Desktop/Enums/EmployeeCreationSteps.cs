b6629a1ab72804d3f4ae95ecf44603bd876c4fba
texno stalin <texno.stalin@gmail.com>
2026-03-13 22:36:56 +0300

    feat: Добавлено новое перечисление для удобной работы с Expander

---
+++ src/apps/LifeLine.HrPanel.Desktop/Enums/EmployeeCreationSteps.cs
@@ -0,0 +1,15 @@
+﻿namespace LifeLine.HrPanel.Desktop.Enums
+{
+    public enum EmployeeCreationSteps
+    {
+        PersonalInfo,
+        Avatar,
+        PersonalDocuments,
+        Address,
+        ContactInformation,
+        EducationDocuments,
+        WorkPermits,
+        Specialties,
+        AssigmentsContracts
+    }
+}
