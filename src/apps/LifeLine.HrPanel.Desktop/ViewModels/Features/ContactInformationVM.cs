e67e4607d0a5bee8702a31545388bb1b4cbf3a47
texno stalin <texno.stalin@gmail.com>
2026-03-13 22:32:28 +0300

    feat: Создан UserControl для контактной информации сотрудника

---
+++ src/apps/LifeLine.HrPanel.Desktop/ViewModels/Features/ContactInformationVM.cs
@@ -0,0 +1,35 @@
+﻿using Shared.WPF.ViewModels.Abstract;
+
+namespace LifeLine.HrPanel.Desktop.ViewModels.Features
+{
+    internal class ContactInformationVM : BaseViewModel
+    {
+        private string _personalPhone = null!;
+        public string PersonalPhone
+        {
+            get => _personalPhone;
+            set => SetProperty(ref _personalPhone, value);
+        }
+
+        private string _personalEmail = null!;
+        public string PersonalEmail
+        {
+            get => _personalEmail;
+            set => SetProperty(ref _personalEmail, value);
+        }
+
+        private string? _corporatePhone;
+        public string? CorporatePhone
+        {
+            get => _corporatePhone;
+            set => SetProperty(ref _corporatePhone, value);
+        }
+
+        private string? _corporateEmail;
+        public string? CorporateEmail
+        {
+            get => _corporateEmail;
+            set => SetProperty(ref _corporateEmail, value);
+        }
+    }
+}
