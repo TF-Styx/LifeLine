e2544156cddcb9493aed79e59d0f12c8aa6e42b5
texno stalin <texno.stalin@gmail.com>
2026-03-13 22:33:00 +0300

    feat: Создан UserControl для персональных документов сотрудника

---
+++ src/apps/LifeLine.HrPanel.Desktop/ViewModels/Features/PersonalDocumentsVM.cs
@@ -0,0 +1,52 @@
+﻿using Shared.Contracts.Response.DirectoryService;
+using Shared.WPF.Commands;
+using Shared.WPF.Constants;
+using Shared.WPF.Helpers;
+using Shared.WPF.Services.FileDialog;
+using Shared.WPF.ViewModels.Abstract;
+
+namespace LifeLine.HrPanel.Desktop.ViewModels.Features
+{
+    internal sealed class PersonalDocumentsVM : BaseViewModel
+    {
+        private readonly IFileDialogService _fileDialogService;
+
+        public PersonalDocumentsVM(IFileDialogService fileDialogService)
+        {
+            _fileDialogService = fileDialogService;
+
+            SelectCommand = new RelayCommand(Execute_SelectCommand);
+        }
+
+        private string _number = null!;
+        public string Number
+        {
+            get => _number;
+            set => SetProperty(ref _number, value);
+        }
+
+        private string? _series;
+        public string? Series
+        {
+            get => _series;
+            set => SetProperty(ref _series, value);
+        }
+
+        private DocumentTypeResponse _documentType = null!;
+        public DocumentTypeResponse DocumentType
+        {
+            get => _documentType;
+            set => SetProperty(ref _documentType, value);
+        }
+
+        public string? FilePath
+        {
+            get => field;
+            set => SetProperty(ref field, value);
+        }
+
+        public RelayCommand SelectCommand { get; private set; }
+        private void Execute_SelectCommand()
+            => FilePath = _fileDialogService.GetFile($"Выберите файл: {FileDialogConsts.PERSONAL_DOCUMENT}", FileFilters.Images);
+    }
+}
