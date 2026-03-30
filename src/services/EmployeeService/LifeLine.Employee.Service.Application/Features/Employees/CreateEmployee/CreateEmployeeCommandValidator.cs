1cdcc9466716a1b5bd7fde83f417c3d59c76213f
texno stalin <texno.stalin@gmail.com>
2026-03-30 16:25:04 +0300

    feat: Создание сотрудника только с нужной информацией о нем

---
+++ src/services/EmployeeService/LifeLine.Employee.Service.Application/Features/Employees/CreateEmployee/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using LifeLine.Employee.Service.Domain.ValueObjects.Employees;
+using Shared.Api.Application.Validators.Realization;
+
+namespace LifeLine.Employee.Service.Application.Features.Employees.CreateEmployee
+{
+    public sealed class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+    {
+        public CreateEmployeeCommandValidator()
+            => Include(new FIOValidator<CreateEmployeeCommand>(Surname.MAX_LENGTH, Name.MAX_LENGTH, Patronymic.MAX_LENGTH));
+    }
+}
