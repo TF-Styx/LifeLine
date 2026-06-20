d9c84ddce44251ccf4f506fe05c366db119f7c79
texno stalin <texno.stalin@gmail.com>
2026-06-20 19:51:08 +0300

    feat: Переопределен метод обновления для должностей

--- src/clients/LifeLine.Directory.Service.Client/Services/Position/IPositionService.cs
+++ src/clients/LifeLine.Directory.Service.Client/Services/Position/IPositionService.cs
@@ -1,7 +1,12 @@
-﻿using Shared.Contracts.Response.DirectoryService;
+﻿using Shared.Contracts.Request.DirectoryService.Position;
+using Shared.Contracts.Response.DirectoryService;
 using Shared.Http.Base;
+using Terminex.Common.Results;
 
 namespace LifeLine.Directory.Service.Client.Services.Position
 {
-    public interface IPositionService : IPositionReadOnlyService, IBaseWriteHttpService<PositionResponse, string>;
+    public interface IPositionService : IPositionReadOnlyService, IBaseWriteHttpService<PositionResponse, string>
+    {
+        Task<Result> UpdateAsync(string positionId, UpdatePositionRequest request);
+    }
 }
--- src/clients/LifeLine.Directory.Service.Client/Services/Position/PositionService.cs
+++ src/clients/LifeLine.Directory.Service.Client/Services/Position/PositionService.cs
@@ -1,12 +1,22 @@
-﻿using Shared.Contracts.Response.DirectoryService;
+﻿using Shared.Contracts.Request.DirectoryService.Position;
+using Shared.Contracts.Response.DirectoryService;
 using Shared.Http.Base;
 using System.Net.Http.Json;
+using Terminex.Common.Results;
 
 namespace LifeLine.Directory.Service.Client.Services.Position
 {
     internal sealed class PositionService(HttpClient httpClient, string departmentId) 
         : BaseHttpService<PositionResponse, string>(httpClient, $"api/departments/{departmentId}/positions"), IPositionService
     {
+        public async Task<Result> UpdateAsync(string positionId, UpdatePositionRequest request)
+        {
+            var response = await HttpClient.PutAsJsonAsync($"{Url}/{positionId}", request, JsonSerializerOptions);
+            response.EnsureSuccessStatusCode();
+
+            return Result.Success();
+        }
+
         public async Task<List<PositionResponse>> GetAllPosition()
         {
             var response = await HttpClient.GetAsync($"{Url}/get-all");
