6e8b7d457e65fb2ebbfc9ff857f56535a13be187
texno stalin <texno.stalin@gmail.com>
2026-07-01 17:56:31 +0300

    feat: Добавлен новый `HttpClient` для проверки наличия отделов и дожностей у какого-либо сотрудника в назначениях `AssignmentCheckService`

---
+++ src/clients/LifeLine.Employee.Service.Client/Services/Employee/Assignment/AssignmentCheckService.cs
@@ -0,0 +1,23 @@
+﻿using Terminex.Common.Results;
+
+namespace LifeLine.Employee.Service.Client.Services.Employee.Assignment
+{
+    public sealed class AssignmentCheckService(HttpClient httpClient) : IAssignmentCheckService
+    {
+        public async Task<Result> HasActiveAssignmentsToDepartmentAsync(Guid departmentId, CancellationToken cancellationToken = default)
+        {
+            var response = await httpClient.GetAsync($"api/assignments/checks/department/{departmentId}", cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            return Result.Success();
+        }
+
+        public async Task<Result> HasActiveAssignmentsToPositionAsync(Guid positionId, CancellationToken cancellationToken = default)
+        {
+            var response = await httpClient.GetAsync($"api/assignments/checks/position/{positionId}", cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            return Result.Success();
+        }
+    }
+}
---
+++ src/clients/LifeLine.Employee.Service.Client/Services/Employee/Assignment/IAssignmentCheckService.cs
@@ -0,0 +1,10 @@
+﻿using Terminex.Common.Results;
+
+namespace LifeLine.Employee.Service.Client.Services.Employee.Assignment
+{
+    public interface IAssignmentCheckService
+    {
+        Task<Result> HasActiveAssignmentsToDepartmentAsync(Guid departmentId, CancellationToken cancellationToken = default);
+        Task<Result> HasActiveAssignmentsToPositionAsync(Guid positionId, CancellationToken cancellationToken = default);
+    }
+}
