cee34012002c79d8b863dd32222bb5b31e9fa914
texno stalin <texno.stalin@gmail.com>
2026-07-27 20:30:17 +0300

    refactor: Возмоден пустой список

--- src/shared/core/Shared.Contracts/Response/EmployeeService/EmployeeHrItemResponse.cs
+++ src/shared/core/Shared.Contracts/Response/EmployeeService/EmployeeHrItemResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace Shared.Contracts.Response.EmployeeService
 {
-    public sealed record EmployeeHrItemResponse(string Id, string Surname, string Name, string? Patronymic, string? PersonalPhoto, bool IsActive, List<AssignmentResponseInfo> Assignments);
+    public sealed record EmployeeHrItemResponse(string Id, string Surname, string Name, string? Patronymic, string? PersonalPhoto, bool IsActive, List<AssignmentResponseInfo>? Assignments);
 
     public sealed record AssignmentResponseInfo(string BranchId, string DepartmentId, string PositionId, string StatusId);
 }
