5835f6854180aefdc79b0d634cdedcf97627c09d
texno stalin <texno.stalin@gmail.com>
2026-07-31 17:53:20 +0300

    feat: Агрегация `UserControl` подписка на события

--- src/apps/LifeLine.HrPanel.Desktop/ViewModels/Features/ManagementEmployee/Employee/EmployeeManagementVM.cs
+++ src/apps/LifeLine.HrPanel.Desktop/ViewModels/Features/ManagementEmployee/Employee/EmployeeManagementVM.cs
@@ -1,10 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LifeLine.HrPanel.Desktop.Models;
+using Shared.WPF.ViewModels.Abstract;
 
 namespace LifeLine.HrPanel.Desktop.ViewModels.Features.ManagementEmployee.Employee
 {
-    internal class EmployeeManagementVM
+    internal sealed class EmployeeManagementVM : BaseViewModel, IAsyncInitializable
     {
+        public EmployeeListVM ListVM { get; }
+        public EmployeeEditVM EditVM { get; }
+
+        private readonly ManagementEmployeeStateService _stateService;
+
+        public EmployeeManagementVM(EmployeeListVM listVM, EmployeeEditVM editVM, ManagementEmployeeStateService stateService)
+        {
+            ListVM = listVM;
+            EditVM = editVM;
+
+            _stateService = stateService;
+
+            ListVM.RequestEdit = OnEditAsync;
+            EditVM.OnClosed = CloseEditPanel;
+
+            EditVM.PersonalInfo!.EmployeeSaved = async () =>
+            {
+                if (_stateService.EmployeeHr != null)
+                {
+                    await ListVM.UpdateEmployees(_stateService.EmployeeHr);
+                    EditVM.ClearFormFields();
+                }
+            };
+
+            EditVM.PersonalPhoto!.OnPhotoUpdated = async () =>
+            {
+                if (_stateService.EmployeeHr != null)
+                    await ListVM.RefreshEmployeePhotoInListAsync();
+            };
+        }
+
+        private bool _isEditPanelVisible;
+        public bool IsEditPanelVisible
+        {
+            get => _isEditPanelVisible;
+            set => SetProperty(ref _isEditPanelVisible, value);
+        }
+
+        private async Task OnEditAsync(EmployeeHrDisplay? display)
+        {
+            EditVM.ClearForm();
+
+            if (display != null)
+                await EditVM.LoadEmployeeAsync(display);
+
+            IsEditPanelVisible = true;
+        }
+
+        private void CloseEditPanel()
+        {
+            EditVM.ClearForm();
+            IsEditPanelVisible = false;
+        }
+
+        async Task IAsyncInitializable.InitializeAsync()
+        {
+            await ListVM.InitializeAsync();
+        }
     }
 }
